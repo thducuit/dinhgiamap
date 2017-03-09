ae3c67042b80048ffd06dda56f36f943da40dd20
hoangtuan438 <hoangtuan438@gmail.com>
2017-03-09 14:46:11 +0700

    sua giao dien va them nut cho popup co 4 nut

--- app/views/partials/popup.blade.php
+++ app/views/partials/popup.blade.php
@@ -1,4 +1,32 @@
-<!-- Modal -->
+<div id="modal_info" class="modal fade modal_popup" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <a href="#" class="close-btn" data-dismiss="modal"></a>
+            <div class="page_03 text-center">
+                <div class="head-top">
+                    <strong><img class="modal_info__marker" src="{{ URL::asset('default/images/map.png') }}" > 
+                      <div class="pp_address">
+                        <p>Địa chỉ : 27 Phó Đức Chính, P.Nguyễn Thái Bình, Q.1, TPHCM</p>
+                      </div>
+                    </strong>
+                </div>
+                <div class="btn-list">
+                    <div class="col col-md-6 col-sm-12 col-xs-12">
+                        <a id="show-price-pop-up" href="#" class="orange-btn even"><img src="{{ URL::asset('default/images/w1.png') }}"> XEM ĐƠN GIÁ ĐẤT</a>
+                        <a class="orange-btn odd btn_dinhgia" ><img src="{{ URL::asset('default/images/w3.png') }}"> THẨM ĐỊNH GIÁ</a>
+                        <a href="" class="orange-btn even"><img src="{{ URL::asset('default/images/w5.png') }}"> XEM QUY HOẠCH</a>
+                    </div>
+                    <div class="col col-md-6 col-sm-12 col-xs-12">
+                        <a class="emeral-btn odd show-price-temp-pop-up"><img src="{{ URL::asset('default/images/w2.png') }}"> ĐỊNH GIÁ SƠ BỘ</a>
+                        <a class="emeral-btn even" href="{{ URL::to('/tai-san-dang-giao-dich.html') }}"><img src="{{ URL::asset('default/images/w4.png') }}"> TÀI SẢN ĐANG GIAO DỊCH</a>
+                        <a href="{{ URL::to('/xem-quy-hoach.html') }}" class="emeral-btn odd"><img src="{{ URL::asset('default/images/w6.png') }}"> ĐỊNH GIÁ DỰ ÁN </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php /*
 <div id="modal_info" class="modal fade" role="dialog">
 	<div class="modal-dialog" style="max-width: 480px;">
 		<div class="modal-content">
@@ -46,4 +74,6 @@
 			</div>
 
 		</div>
-			</div><!--end modal-->
+			</div>
+ * 
+ */?>
